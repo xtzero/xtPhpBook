863f5526fae635a335d72fc67de9514f6c94e299
xt <t@xtzero.me>
2018-10-05 15:07:13 +0800

    更新了表达式运算符

---
+++ code/array.php
@@ -0,0 +1,9 @@
+<?php
+// $arr = ['username' => 'abc123','password' => 'pwd123'];
+// var_dump($arr);
+
+$str = 'apple,grape,banana,pear';
+$fruits = explode(',',$str);
+var_dump($str);
+echo '<br/>';
+var_dump($fruits);
